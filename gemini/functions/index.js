59bd9e34c79c82739e3ce7ab5620097db323eaac
PamornT <chocobobu@gmail.com>
2024-12-08 13:59:18 +0700

    Update index.js
    change context for BU Workshop

--- gemini/functions/index.js
+++ gemini/functions/index.js
@@ -8,7 +8,6 @@ const gemini = require("./utils/gemini");
 
 const NodeCache = require("node-cache");
 const cache = new NodeCache();
-const CACHE_IMAGE = "image_";
 const CACHE_CHAT = "chat_";
 
 exports.webhook = onRequest(async (req, res) => {
@@ -20,38 +19,29 @@ exports.webhook = onRequest(async (req, res) => {
       case "message":
         if (event.message.type === "text") {
           const prompt = event.message.text;
-          
-          /* 3. Generate text from text-and-image input (multimodal) */
-          // 3.5. Get cache image
-          // 3.6. Check available cache
-          // 3.7. Send a prompt to Gemini multimodal
-          // 3.8. Reply a generated text
+         
+/* 1. Generate text from text-only input */
+// 1.1. Send a prompt to Gemini
+          // const text = await gemini.textOnly(prompt)
+// 1.2. Reply a generated text
+          // await line.reply(event.replyToken, [{type: 'text', text:text}])
 
-          /* 1. Generate text from text-only input */
-          // 1.1. Send a prompt to Gemini
-          // 1.2. Reply a generated text
-
-          /* 2. Generate text from text-only input with contextual info */
-          // 2.1. Send a prompt to Gemini
-          // 2.2. Reply a generated text
-
-          /* 4. Build multi-turn conversations (chat) */
-          // 4.1. Get a cache chat history
-          // 4.2. Check available cache
-          // 4.3. Send a prompt to Gemini
-          // 4.4. Reply a generated text
-          // 4.5. Push a new chat history
-          // Note: From Mar 2024, the spec of parts value has changed to [{ text: xxx }]
-          // 4.6. Set a cache chat history
-          break;
-        }
-
-        if (event.message.type === "image") {
-          /* 3. Generate text from text-and-image input (multimodal) */
-          // 3.1. Get an image binary
-          // 3.2. Convert binary to base64
-          // 3.3. Set a cache image
-          // 3.4. Ask for prompt
+/* 2. Build multi-turn conversations (chat) */
+// 2.1. Get a cache chat history
+          // let chatHistory = cache.get(CACHE_CHAT + userId)
+// 2.2. Check available cache
+          // if (!chatHistory) {
+          //   chatHistory = []
+          // }
+// 2.3. Send a prompt to Gemini
+          // const text = await gemini.chat(chatHistory, prompt)
+// 2.4. Reply a generated text
+          // await line.reply(event.replyToken, [{type: 'text', text: text}])
+// 2.5. Push a new chat history
+          // chatHistory.push({ role: 'user', parts: [{ text: prompt }] })
+          // chatHistory.push({ role: 'model', parts: [{ text: text }] })
+// 2.6. Set a cache chat history
+          // cache.set(CACHE_CHAT + userId, chatHistory, 60)
           break;
         }
         break;
